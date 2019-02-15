ef8a1a6587511225c6b648c18a7f82a171e91875
Tuan Mai <tuandinhmai@gmail.com>
2019-02-14 23:34:45 -0500

    Added files to incorporate functionality of reading DICOM files.

---
+++ ThesisHoloLens/Assets/Scripts/DicomFile.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DicomFile : MonoBehaviour
+{
+
+	
+    void GetDicomFileCTInfo() { }
+
+    DicomFile() { }
+
+    bool Load(string sFileName) { return false; }
+
+    void Init() { }
+  
+    bool m_bInterpolationFileFlag;
+    // POI/ROI/RTPLAN/CT
+    int m_iDicomFileType;
+
+    // Instance number for Dicom file
+    // for CT:     set it to the value of tag (0x0020,0x0013)
+    // for POI:    set it to BBT_INSTANCE_NUM_POI
+    // for ROI:    set it to BBT_INSTANCE_NUM_ROI
+    // for RTPLAN: set it to BBT_INSTANCE_NUM_RTPLAN
+    int m_iInstanceNum;     // instance number
+
+    // (0008, 0016)	UI	SOP Class UID
+    // (0008, 0018)	UI	SOP Instance UID
+    string m_sSOPClassUID;
+    string m_sSOPInstanceUID;
+
+    // Image Rows		(0028, 0010)
+    // Image Columns	(0028, 0011)
+    int m_iImgRow;
+    int m_iImgCol;
+
+    // Patient ID (0010, 0020)
+    string m_sPatientID;
+
+    // Patient Name (0010,0010)
+    string m_sPatientNameFirst;
+    string m_sPatientNameLast;
+
+    // Patient Sex (0010, 0040)
+    string m_sPatientSex;
+
+    // image position (patient) tag (0x0020, 0x0032)
+    double[] m_dImgPos=new double[3];  // 0:X  1:Y  2:Z
+
+    // image XY pixel spacing tag (0x0028, 0x0030)  // unit mm
+    double m_dImgXPixelSpacing;
+    double m_dImgYPixelSpacing;
+    // add on oct 29 
+    double m_dZdistance;
+    // Slice thickness tag (0x0018, 0x0050)  // unit mm
+    double m_dSliceThicknessOriginal;
+
+    // Slice thickness calculated by image z Position
+    double m_dSliceThickness;
+
+    // Slice Location tag (0x0020, 0x1041)
+    double m_dSliceLocation;
+
+    // window width and window center 
+    double m_dWinCenter;    // (0x0028, 0x1050)
+    double m_dWinWidth;     // (0x0028, 0x1051)
+
+  
+
+    // CT Pixel Data length and pointer (7FE0,0010)
+    ulong m_ulPixelDataLen;
+    byte[] m_pPixelData;
+    double m_pEdgeDataSobel;
+    byte[] m_pEdgeData;
+
+   
+    
+    bool ReadRecord(DicomFileRecord pRecord)
+    {
+
+
+        return true;
+    }
+    void Free()
+    {
+
+    }
+
+    bool m_bExplicitVR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ThesisHoloLens/Assets/Scripts/DicomFileRecord.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DicomFileRecord : MonoBehaviour
+{
+
+    void Free()
+    {
+
+    }
+
+    void Init()
+    {
+
+    }
+
+    int GetRecordLen() {
+        return 0;
+    }
+
+    DicomFileRecord() { }
+    
+
+    ushort m_usGrp;
+    ushort m_usEle;
+    ulong m_ulLen;
+    string m_sVR;
+
+    // this is the byte length in the dicom file including
+    // group		: 2 bytes:
+    // element		: 2 bytes
+    // VR			: 0 or 2 bytes 
+    // data length	: 4 or 2 bytes 
+    ushort m_usTagLen;
+    long m_iFilePos;
+
+    string m_sName;
+    char[] m_pData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
